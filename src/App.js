8f37bbaef411b73c868930c339a78529ac63c9be
vikash <champvikash4@gmail.com>
2022-04-16 13:13:44 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Body from './Components/Body';
+// import Footbar from './Components/Footbar';
 
 
 function App() {
   return (
    <>
    <Navbar/>
+   <Body/>
+   {/* <Footbar/> */}
    </>
   );
 }
---
+++ src/Components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react';
+class Body extends React.Component {
+
+    render() { 
+        return (
+            <>
+        <div className='bg-color-dark , hight:1000vh'>
+
+        </div>
+            </>
+        );
+    }
+}
+ 
+export default Body ;
